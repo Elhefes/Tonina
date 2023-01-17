dfc1e5f3abaf0783cbfa720d9d193d0d8cf158cb
henripal <henri.j.palin@gmail.com>
2023-01-17 19:39:47 +0200

    add basic first scene and player movement

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
