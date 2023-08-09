313297364ded7488c72097ed23f77c77b499e5e3
SummaS99 <LeipuriHiiva1@gmail.com>
2023-08-09 18:14:25 +0300

    fix spear facing wrong way when thrown

--- Assets/Scripts/Weapons/Projectiles/Projectile.cs
+++ Assets/Scripts/Weapons/Projectiles/Projectile.cs
@@ -9,8 +9,7 @@ public class Projectile : Weapon
     {
         animator.SetTrigger("SpearAttack");
 
-        var quack = new Quaternion(shootingPoint.rotation.w, shootingPoint.rotation.eulerAngles.x, shootingPoint.rotation.eulerAngles.y, shootingPoint.rotation.eulerAngles.z);
-        GameObject projectile = Instantiate(gameObject, shootingPoint.position, quack);
+        GameObject projectile = Instantiate(gameObject, shootingPoint.position, shootingPoint.rotation);
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * projectileForce, ForceMode.Impulse);
         print("niin");
