0f2bf28242c03a423604aee5f2108184fa626f38
SummaS99 <LeipuriHiiva1@gmail.com>
2023-08-09 13:55:17 +0300

    fix spear model's rotation

--- Assets/Scripts/Weapons/Projectiles/Projectile.cs
+++ Assets/Scripts/Weapons/Projectiles/Projectile.cs
@@ -9,7 +9,7 @@ public class Projectile : Weapon
     {
         animator.SetTrigger("SpearAttack");
 
-        var quack = new Quaternion(shootingPoint.rotation.w, shootingPoint.rotation.eulerAngles.x, shootingPoint.rotation.eulerAngles.y, shootingPoint.rotation.eulerAngles.z + 90f);
+        var quack = new Quaternion(shootingPoint.rotation.w, shootingPoint.rotation.eulerAngles.x, shootingPoint.rotation.eulerAngles.y, shootingPoint.rotation.eulerAngles.z);
         GameObject projectile = Instantiate(gameObject, shootingPoint.position, quack);
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.AddForce(transform.forward * projectileForce, ForceMode.Impulse);
