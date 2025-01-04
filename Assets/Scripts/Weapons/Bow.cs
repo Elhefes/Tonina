caf15f3100fc8d0be3ec730332bdedea48aa3964
SummaS99 <LeipuriHiiva1@gmail.com>
2025-01-04 17:20:42 +0200

    arrows hit only one enemy

--- Assets/Scripts/Weapons/Bow.cs
+++ Assets/Scripts/Weapons/Bow.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class Bow : Projectile
 {
-    public Rigidbody arrow;
+    public Arrow arrow;
+    public Rigidbody arrowRb;
 
     public override void Attack(Animator animator)
     {
@@ -12,20 +13,20 @@ public class Bow : Projectile
 
     void SpawnArrow()
     {
-        directorComponent.SetActive(true);
+        arrow.directorComponent.SetActive(true);
+        arrow.canHit = true;
 
         quantity--;
         if (quantity < 1) notAvailable = true;
         if (weaponWheelQuantityTMP != null) weaponWheelQuantityTMP.text = quantity.ToString();
 
-        arrow.constraints = RigidbodyConstraints.None;
+        arrowRb.constraints = RigidbodyConstraints.None;
 
-        Instantiate(arrow, shootingPoint.position, shootingPoint.rotation);
-        arrow.constraints = RigidbodyConstraints.FreezeAll;
-        directorComponent.SetActive(false);
+        Instantiate(arrow.gameObject, shootingPoint.position, shootingPoint.rotation);
+        arrowRb.constraints = RigidbodyConstraints.FreezeAll;
+        arrow.directorComponent.SetActive(false);
+        arrow.canHit = false;
         // Need to find a way to disable and enable arrow after shooting
         //arrow.gameObject.SetActive(false);
-
-        // Also make it so that arrows hit only 1 enemy
     }
 }
---
+++ Assets/Scripts/Weapons/Projectiles/Arrow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Arrow : Projectile
+{
+    public GameObject hitSoundObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!canHit || !directorComponent.activeSelf) return;
+
+        base.HandleCollision(other.gameObject);
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.gameObject.GetComponent<Enemy>();
+            enemy.SlowDownEnemy();
+            // TODO: add hit sounds
+            //hitSoundObject.SetActive(true);
+            //hitSoundObject.transform.SetParent(null);
+            Destroy(gameObject);
+        }
+    }
+}
