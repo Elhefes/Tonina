b5594e12eef590b3a450f6efa9685c5a8e49afc9
SummaS99 <LeipuriHiiva1@gmail.com>
2025-05-23 12:09:10 +0300

    village middle gate position updates when new floors are built

--- Assets/Scripts/UI/VillageTeleportMenu.cs
+++ Assets/Scripts/UI/VillageTeleportMenu.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class VillageTeleportMenu : MonoBehaviour
 {
     public Transform[] gateTransforms;
+    public GameObject middleGate;
+    public Vector3[] middleGatePositionsFrom4rdTo14th;
+    public Vector3[] middleGateTransformPositionsFrom4rdTo14th;
     public GameObject[] blueBacks;
     public Player player;
     private int gateIndex;
+    private int extraFloorsBuilt;
+
+    private void Start()
+    {
+        // TODO: get extraFloorsBuilt when save/load works
+        if (extraFloorsBuilt > 0) UpdateMiddleGatePosition();
+    }
 
     private void OnEnable()
     {
@@ -19,6 +29,14 @@ public class VillageTeleportMenu : MonoBehaviour
         else if (Vector3.Distance(player.transform.position, gateTransforms[0].position) < 5f) gateIndex = 1;
         else if (Vector3.Distance(player.transform.position, gateTransforms[1].position) < 5f) gateIndex = 2;
         else if (Vector3.Distance(player.transform.position, gateTransforms[2].position) < 5f) gateIndex = 3;
+    }
+
+    public void IncrementExtraFloorsBuilt() { extraFloorsBuilt++; } // Temporary
+
+    public void UpdateMiddleGatePosition()
+    {
+        middleGate.transform.position = middleGatePositionsFrom4rdTo14th[extraFloorsBuilt - 1];
+        gateTransforms[1].position = middleGateTransformPositionsFrom4rdTo14th[extraFloorsBuilt - 1];
     }
 
     void UpdateBlueBacks()
