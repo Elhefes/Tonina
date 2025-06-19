33ee4859ff140f6ce8454e313b0e88512d0a1a2f
SummaS99 <LeipuriHiiva1@gmail.com>
2025-06-19 18:39:08 +0300

    small placeable buildings can be placed on grass

--- Assets/Scripts/BuildingPlacing.cs
+++ Assets/Scripts/BuildingPlacing.cs
@@ -5,6 +5,7 @@ public class BuildingPlacing : MonoBehaviour
     public PlaceableBuilding placeableBuildingPrefab;
     public Material placingMat;
     public bool canPlace;
+    public bool canPlaceOnGrass;
     Color canPlaceColor = new Color(58f / 255f, 189f / 255f, 105f / 255f, 99f / 255f); // Green jade color
     Color cannotPlaceColor = new Color(255f / 255f, 0f / 255f, 0f / 255f, 99f / 255f);  // Red color
 
@@ -34,14 +35,26 @@ public class BuildingPlacing : MonoBehaviour
 
     private bool isPlaceable()
     {
-        if (Physics.OverlapBox(transform.position, halfExtentsVector, Quaternion.Euler(0f, 0f, 0f), LayerMask.GetMask("GroundForBuilding")).Length > 0)
-        {
-            if (Physics.OverlapBox(transform.position, halfExtentsVector, transform.rotation, collisionLayerMask).Length > 0)
-            {
-                return false;
-            }
-        }
-        else return false;
-        return true;
+        string[] groundLayers = canPlaceOnGrass
+            ? new[] { "GroundForBuilding", "Terrain" }
+            : new[] { "GroundForBuilding" };
+
+        bool isOnValidGround = Physics.OverlapBox(
+            transform.position,
+            halfExtentsVector,
+            Quaternion.identity,
+            LayerMask.GetMask(groundLayers)
+        ).Length > 0;
+
+        if (!isOnValidGround) return false;
+
+        bool isColliding = Physics.OverlapBox(
+            transform.position,
+            halfExtentsVector,
+            transform.rotation,
+            collisionLayerMask
+        ).Length > 0;
+
+        return !isColliding;
     }
 }
