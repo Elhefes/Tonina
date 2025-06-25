4e73cdd9941dc8cca54778b13e5f9c5dccea4e34
SummaS99 <LeipuriHiiva1@gmail.com>
2025-06-25 15:16:12 +0300

    add diagonal movement to bird shadows

--- Assets/Scripts/BirdShadowSpawner.cs
+++ Assets/Scripts/BirdShadowSpawner.cs
@@ -6,6 +6,13 @@ public class BirdShadowSpawner : MonoBehaviour
     private Coroutine spawnerCoroutine;
     public GameObject[] shadowObjects;
     public GameObject[] shadowChildren;
+    private Animator[] animators;
+    public AnimationClip[] animationClips;
+
+    private void Start()
+    {
+        animators = GetComponentsInChildren<Animator>(includeInactive: true);
+    }
 
     void OnEnable()
     {
@@ -16,7 +23,7 @@ public class BirdShadowSpawner : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(5f, 10f));
+            yield return new WaitForSeconds(Random.Range(3.75f, 7.5f));
             int r = Random.Range(0, shadowObjects.Length);
             if (shadowObjects[r].activeSelf)
             {
@@ -27,13 +34,21 @@ public class BirdShadowSpawner : MonoBehaviour
                     shadowObjects[r].SetActive(true);
 
                     shadowObjects[r].transform.localPosition = new Vector3(0f, 0f, Random.Range(-40f, 30f));
+                    PlayRandomAnim(r);
                 }
             }
             else
             {
                 shadowObjects[r].SetActive(true);
                 shadowObjects[r].transform.localPosition = new Vector3(0f, 0f, Random.Range(-40f, 30f));
+                PlayRandomAnim(r);
             }
         }
+    }
+
+    private void PlayRandomAnim(int r)
+    {
+        string stateName = animationClips[Random.Range(0, animationClips.Length)].name;
+        animators[r].Play(stateName);
     }
 }
