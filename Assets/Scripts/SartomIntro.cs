0e54a17072ec7ba663bf7445af7fecf0fa791a30
SummaS99 <LeipuriHiiva1@gmail.com>
2025-07-13 10:48:06 +0300

    add simple transition to playerpivot in intro scene

--- Assets/Scripts/SartomIntro.cs
+++ Assets/Scripts/SartomIntro.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class SartomIntro : MonoBehaviour
 {
     public Animator animator;
+    public GameObject playerPivot;
+    public GameObject staticTwinStatue;
 
     public void CallTrigger(string trigger)
     {
         animator.SetTrigger(trigger);
+    }
+
+    public void EnableOnlyBattleModeObjects()
+    {
+        playerPivot.SetActive(true);
+        staticTwinStatue.SetActive(true);
+        gameObject.SetActive(false);
     }
 
     void LegsTimingTrigger()
