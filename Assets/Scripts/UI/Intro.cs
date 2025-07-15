625dbe6ff9151fdc07f806a2b4fa9278c7a16ed7
SummaS99 <LeipuriHiiva1@gmail.com>
2025-07-15 16:18:27 +0300

    music fades down before transition from intro graphic to intro scene

--- Assets/Scripts/UI/Intro.cs
+++ Assets/Scripts/UI/Intro.cs
@@ -3,14 +3,32 @@ using UnityEngine;
 public class Intro : MonoBehaviour
 {
     public MusicPlayer musicPlayer;
+    private bool fadingMusicDown;
+    private float musicVolumeAtStart;
+    private float musicFadeIncrement;
 
     private void OnEnable()
     {
         musicPlayer.PlayIntroMusic();
+        musicVolumeAtStart = musicPlayer.audioSource.volume;
     }
 
     public void EndIntro()
     {
         SceneChangingManager.Instance.LoadScene("IntroScene");
+    }
+
+    public void StartFadingMusicDown()
+    {
+        musicFadeIncrement = musicVolumeAtStart / 250f; // 2.5 Seconds in FixedUpdate
+        fadingMusicDown = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (fadingMusicDown)
+        {
+            musicPlayer.audioSource.volume -= musicFadeIncrement;
+        }
     }
 }
