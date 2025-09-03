14472a5a63485be82b32bbc5d50da6d54a6335aa
SummaS99 <LeipuriHiiva1@gmail.com>
2025-09-03 10:34:02 +0300

    attack mode add warriors text updates with spawn numbers

--- Assets/Scripts/UI/AttackModeSpawnController.cs
+++ Assets/Scripts/UI/AttackModeSpawnController.cs
@@ -3,8 +3,11 @@ using TMPro;
 
 public class AttackModeSpawnController : MonoBehaviour
 {
+    public int maxFriendliesAmount;
+    private int currentFriendliesAmount;
     public int[] spawnArray;
     private int selectedSpawnNumber;
+    public TMP_Text addText;
     public TMP_Text[] spawnTexts;
     public GameObject amountButtons;
 
@@ -12,18 +15,29 @@ public class AttackModeSpawnController : MonoBehaviour
     {
         selectedSpawnNumber = 4;
         UpdateSpawnArray();
+        UpdateAddText();
     }
 
     public void IncreaseSpawn()
     {
-        spawnArray[selectedSpawnNumber]++;
-        UpdateSpawnArray();
+        if (currentFriendliesAmount < maxFriendliesAmount)
+        {
+            spawnArray[selectedSpawnNumber]++;
+            currentFriendliesAmount++;
+            UpdateSpawnArray();
+            UpdateAddText();
+        }
     }
 
     public void DecreaseSpawn()
     {
-        if (spawnArray[selectedSpawnNumber] > 0) spawnArray[selectedSpawnNumber]--;
-        UpdateSpawnArray();
+        if (spawnArray[selectedSpawnNumber] > 0)
+        {
+            spawnArray[selectedSpawnNumber]--;
+            currentFriendliesAmount--;
+            UpdateSpawnArray();
+            UpdateAddText();
+        }
     }
 
     public void UpdateSelectedSpawnNumber(int spawnNumber)
@@ -41,6 +55,18 @@ public class AttackModeSpawnController : MonoBehaviour
         for (int i = 0; i < spawnArray.Length; i++)
         {
             spawnTexts[i].text = spawnArray[i].ToString();
+        }
+    }
+
+    void UpdateAddText()
+    {
+        if (currentFriendliesAmount == maxFriendliesAmount)
+        {
+            addText.text = "All spawns selected!";
+        }
+        else
+        {
+            addText.text = "TT Warrior spawns:\nAdd " + (maxFriendliesAmount - currentFriendliesAmount) + " more!";
         }
     }
 }
