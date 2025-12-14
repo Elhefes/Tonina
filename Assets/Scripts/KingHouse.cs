faf1f53a47a372ebd7874c0088790dcbf66f2b3a
SummaS99 <LeipuriHiiva1@gmail.com>
2025-12-14 12:24:10 +0200

    king house nameplate updates with players names

--- Assets/Scripts/KingHouse.cs
+++ Assets/Scripts/KingHouse.cs
@@ -4,4 +4,21 @@ public class KingHouse : MonoBehaviour
 {
     public Vector3 playerSpawnPosition;
     public Vector3 battleFieldStartingPosition;
+    public GameObject namePlate;
+
+    private void OnEnable()
+    {
+        UpdateNamePlate();
+    }
+
+    public void UpdateNamePlate()
+    {
+        if (namePlate != null)
+        {
+            int selectedSave = PlayerPrefs.GetInt("selectedSaveFile", 1);
+            if (selectedSave == 1) namePlate.name = "King " + PlayerPrefs.GetString("playerName1", "");
+            else if (selectedSave == 2) namePlate.name = "King " + PlayerPrefs.GetString("playerName2", "");
+            else if (selectedSave == 3) namePlate.name = "King " + PlayerPrefs.GetString("playerName3", "");
+        }
+    }
 }
