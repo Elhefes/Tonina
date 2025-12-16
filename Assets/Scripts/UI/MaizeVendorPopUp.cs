ffee6c1f59d311f3ec1968e37023818edfd29622
SummaS99 <LeipuriHiiva1@gmail.com>
2025-12-16 10:09:00 +0200

    fix maize vendor UI persistence with maize production level

--- Assets/Scripts/UI/MaizeVendorPopUp.cs
+++ Assets/Scripts/UI/MaizeVendorPopUp.cs
@@ -5,28 +5,39 @@ public class MaizeVendorPopUp : MonoBehaviour
     public GameObject[] textObjects;
     public MaizeHandler maizeHandler;
 
+    private void OnEnable()
+    {
+        foreach (GameObject obj in textObjects) obj.SetActive(false);
+        int m = GameState.Instance.progressionData.maizeProductionLevel;
+        if (m > 0)
+        {
+            if (m < textObjects.Length) textObjects[m].SetActive(true);
+        }
+        else textObjects[0].SetActive(true);
+    }
+
     public void BuyMoreStartingMaize()
     {
         if (GameState.Instance.progressionData.maizeProductionLevel >= textObjects.Length) return;
 
         GameState.Instance.progressionData.maizeProductionLevel++;
-        int startingMaizeAmount = GameState.Instance.progressionData.maizeProductionLevel;
+        int newMaizeProductionLevel = GameState.Instance.progressionData.maizeProductionLevel;
 
         foreach (GameObject obj in textObjects) obj.SetActive(false);
-        if (startingMaizeAmount < textObjects.Length) textObjects[startingMaizeAmount].SetActive(true);
+        if (newMaizeProductionLevel < textObjects.Length) textObjects[newMaizeProductionLevel].SetActive(true);
 
-        SetMaizeProductionLevel(startingMaizeAmount);
+        SetMaizeProductionLevel(newMaizeProductionLevel);
         GameState.Instance.SaveWorld();
     }
 
-    private void SetMaizeProductionLevel(int startingMaizeAmount)
+    private void SetMaizeProductionLevel(int newMaizeProductionLevel)
     {
         // TODO: Make Maize Place incrementally cheaper also
-        if (startingMaizeAmount == 3)
+        if (newMaizeProductionLevel == 3)
         {
             // MP Price = 120
         }
-        else if (startingMaizeAmount == 3)
+        else if (newMaizeProductionLevel == 4)
         {
             // MP Price = 100
         }
