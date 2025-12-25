252e9bd31666cfb762f6bc35446ce11b684cce3f
SummaS99 <LeipuriHiiva1@gmail.com>
2025-12-25 20:58:23 +0200

    maize place is unlocked from maize vendor

--- Assets/Scripts/UI/MaizeVendorPopUp.cs
+++ Assets/Scripts/UI/MaizeVendorPopUp.cs
@@ -3,17 +3,42 @@ using UnityEngine;
 public class MaizeVendorPopUp : MonoBehaviour
 {
     public GameObject[] textObjects;
+    public GameObject maizePlaceBuyMenu;
+    public GameObject buyMoreMaizeButton;
     public MaizeHandler maizeHandler;
 
     private void OnEnable()
     {
         foreach (GameObject obj in textObjects) obj.SetActive(false);
+        if (!GameState.Instance.progressionData.maizePlaceUnlocked)
+        {
+            EnableBuyMaizePlaceMenu(true);
+            return;
+        }
         int m = GameState.Instance.progressionData.maizeProductionLevel;
         if (m > 0)
         {
             if (m < textObjects.Length) textObjects[m].SetActive(true);
         }
-        else textObjects[0].SetActive(true);
+        else
+            {
+                textObjects[0].SetActive(true);
+            }
+        EnableBuyMaizePlaceMenu(false);
+    }
+
+    private void EnableBuyMaizePlaceMenu(bool value)
+    {
+        maizePlaceBuyMenu.SetActive(value);
+        buyMoreMaizeButton.SetActive(!value);
+    }
+
+    public void BuyMaizePlace()
+    {
+        GameState.Instance.progressionData.maizePlaceUnlocked = true;
+        GameState.Instance.SaveWorld();
+        EnableBuyMaizePlaceMenu(false);
+        textObjects[0].SetActive(true);
     }
 
     public void BuyMoreStartingMaize()
