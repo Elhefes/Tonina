310856a6e7741ffbe2eeacefe683a05a5206493d
SummaS99 <LeipuriHiiva1@gmail.com>
2026-01-13 20:28:08 +0200

    update intro ending texts

--- Assets/Scripts/UI/IntroEndTexts.cs
+++ Assets/Scripts/UI/IntroEndTexts.cs
@@ -5,7 +5,7 @@ using TMPro;
 public class IntroEndTexts : MonoBehaviour
 {
     public TMP_Text welcomeTMP;
-    public TMP_Text revolutionTMP;
+    public TMP_Text jadeTMP;
     private string fullText;
     private string currentText = "";
 
@@ -28,15 +28,15 @@ public class IntroEndTexts : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
 
-        fullText = revolutionTMP.text;
-        revolutionTMP.text = "";
-        revolutionTMP.gameObject.SetActive(true);
+        fullText = jadeTMP.text;
+        jadeTMP.text = "";
+        jadeTMP.gameObject.SetActive(true);
 
         for (int i = 0; i < fullText.Length + 1; i++)
         {
             currentText = fullText.Substring(0, i);
             yield return new WaitForSeconds(0.15f);
-            revolutionTMP.text = currentText;
+            jadeTMP.text = currentText;
         }
 
         yield return new WaitForSeconds(3f);
