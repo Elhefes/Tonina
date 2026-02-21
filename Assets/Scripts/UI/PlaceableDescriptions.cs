177d0603057715cd2fda2c7993b3eefbc6bb914b
SummaS99 <LeipuriHiiva1@gmail.com>
2026-02-21 09:36:08 +0200

    add locked placeable descriptions to battlefield menu placeables info

--- Assets/Scripts/UI/PlaceableDescriptions.cs
+++ Assets/Scripts/UI/PlaceableDescriptions.cs
@@ -2,10 +2,33 @@ using UnityEngine;
 
 public class PlaceableDescriptions : MonoBehaviour
 {
-    public GameObject[] descriptionObjects;
+    public GameObject[] descriptionParentObjects;
+    public GameObject[] unlockedDescriptionObjects;
+    public GameObject[] lockedDescriptionObjects;
 
-    public void ResetDescriptionObjects()
+    private void OnEnable()
     {
-        foreach (GameObject obj in descriptionObjects) obj.SetActive(false);
+        UpdateDescriptionObjects();
+    }
+
+    private void UpdateDescriptionObjects()
+    {
+        foreach (GameObject obj in unlockedDescriptionObjects) obj.SetActive(false);
+        foreach (GameObject obj in lockedDescriptionObjects) obj.SetActive(false);
+
+        // TODO: Add locked descriptions for fence and spear rack when they are locked
+
+        for (int i = 0; i < unlockedDescriptionObjects.Length; i++)
+        {
+            if (i == 2 && !GameState.Instance.progressionData.maizePlaceUnlocked) lockedDescriptionObjects[0].SetActive(true);
+            else if (i == 4 && !GameState.Instance.progressionData.fillOkillUnlocked) lockedDescriptionObjects[1].SetActive(true);
+            else if (i == 5 && !GameState.Instance.progressionData.towerUnlocked) lockedDescriptionObjects[2].SetActive(true);
+            else unlockedDescriptionObjects[i].SetActive(true);
+        }
+    }
+
+    public void DisableDescriptionParentObjects()
+    {
+        foreach (GameObject obj in descriptionParentObjects) obj.SetActive(false);
     }
 }
