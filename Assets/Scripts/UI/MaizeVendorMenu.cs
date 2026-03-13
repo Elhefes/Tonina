c6fde1d2c39e91e539ed2d4449386a7c5cb28c2d
SummaS99 <LeipuriHiiva1@gmail.com>
2026-03-13 13:17:16 +0200

    money is updated on maize vendor shop menu

--- Assets/Scripts/UI/MaizeVendorMenu.cs
+++ Assets/Scripts/UI/MaizeVendorMenu.cs
@@ -1,18 +1,23 @@
 using UnityEngine;
 
-public class MaizeVendorPopUp : MonoBehaviour
+public class MaizeVendorMenu : MonoBehaviour
 {
+    public MoneyCounter moneyCounter;
+    public int maizePlaceCost;
+    public int[] maizeEarCosts;
+
     public GameObject[] textObjects;
     public GameObject maizePlaceBuyMenu;
-    public GameObject buyMoreMaizeButton;
     public MaizeHandler maizeHandler;
 
     private void OnEnable()
     {
         foreach (GameObject obj in textObjects) obj.SetActive(false);
+        moneyCounter.UpdateMoneyCounter();
         if (!GameState.Instance.progressionData.maizePlaceUnlocked)
         {
-            EnableBuyMaizePlaceMenu(true);
+            maizePlaceBuyMenu.SetActive(true);
+            moneyCounter.UpdateBuyAvailability(maizePlaceCost);
             return;
         }
         int m = GameState.Instance.progressionData.maizeProductionLevel;
@@ -21,27 +26,29 @@ public class MaizeVendorPopUp : MonoBehaviour
             if (m < textObjects.Length) textObjects[m].SetActive(true);
         }
         else
-            {
-                textObjects[0].SetActive(true);
-            }
-        EnableBuyMaizePlaceMenu(false);
+        {
+            textObjects[0].SetActive(true);
+        }
+        maizePlaceBuyMenu.SetActive(false);
+
+        moneyCounter.UpdateBuyAvailability(maizeEarCosts[m]);
     }
 
-    private void EnableBuyMaizePlaceMenu(bool value)
+    public void BuyThis()
     {
-        maizePlaceBuyMenu.SetActive(value);
-        buyMoreMaizeButton.SetActive(!value);
+        if (maizePlaceBuyMenu.activeSelf)
+        {
+            GameState.Instance.progressionData.maizePlaceUnlocked = true;
+            moneyCounter.ReduceMoney(maizePlaceCost);
+            GameState.Instance.SaveWorld();
+            maizePlaceBuyMenu.SetActive(false);
+            textObjects[0].SetActive(true);
+        }
+        else BuyMoreStartingMaize();
+        moneyCounter.UpdateMoneyCounter();
     }
 
-    public void BuyMaizePlace()
-    {
-        GameState.Instance.progressionData.maizePlaceUnlocked = true;
-        GameState.Instance.SaveWorld();
-        EnableBuyMaizePlaceMenu(false);
-        textObjects[0].SetActive(true);
-    }
-
-    public void BuyMoreStartingMaize()
+    private void BuyMoreStartingMaize()
     {
         if (GameState.Instance.progressionData.maizeProductionLevel >= textObjects.Length) return;
 
@@ -49,9 +56,16 @@ public class MaizeVendorPopUp : MonoBehaviour
         int newMaizeProductionLevel = GameState.Instance.progressionData.maizeProductionLevel;
 
         foreach (GameObject obj in textObjects) obj.SetActive(false);
-        if (newMaizeProductionLevel < textObjects.Length) textObjects[newMaizeProductionLevel].SetActive(true);
 
         SetMaizeProductionLevel(newMaizeProductionLevel);
+        moneyCounter.ReduceMoney(maizeEarCosts[newMaizeProductionLevel - 1]);
+
+        if (newMaizeProductionLevel < textObjects.Length)
+        {
+            textObjects[newMaizeProductionLevel].SetActive(true);
+            moneyCounter.UpdateBuyAvailability(maizeEarCosts[newMaizeProductionLevel]);
+        }
+
         GameState.Instance.SaveWorld();
     }
 
