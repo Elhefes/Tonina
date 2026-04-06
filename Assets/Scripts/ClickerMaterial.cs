8fbd109eafba78c936554ecd9644688e849517ee
SummaS99 <LeipuriHiiva1@gmail.com>
2026-04-06 20:14:16 +0300

    fix negative clicker material alpha value bug

--- Assets/Scripts/ClickerMaterial.cs
+++ Assets/Scripts/ClickerMaterial.cs
@@ -7,19 +7,26 @@ public class ClickerMaterial : MonoBehaviour
 
     Color matColor;
 
-    void Start()
+    private void Start()
     {
         alpha = 1f;
         matColor = clickerMat.color;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        matColor.a = alpha;
-        clickerMat.color = new Color(clickerMat.color.r, clickerMat.color.g, clickerMat.color.b, Mathf.Clamp(alpha, 0f, 1f));
         if (alpha > 0f)
         {
             alpha -= 0.01f;
+            alpha = Mathf.Max(alpha, 0f); // ensures it never goes below 0
         }
+
+        matColor.a = alpha;
+        clickerMat.color = new Color(
+            clickerMat.color.r,
+            clickerMat.color.g,
+            clickerMat.color.b,
+            alpha
+        );
     }
 }
