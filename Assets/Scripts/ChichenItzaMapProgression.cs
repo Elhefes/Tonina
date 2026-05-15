2ad2a7e25c23605f4d81cc1dbe29d183d52ef5ee
SummaS99 <LeipuriHiiva1@gmail.com>
2026-05-15 09:06:00 +0300

    gameplay camera angles change automatically in chichen itza

--- Assets/Scripts/ChichenItzaMapProgression.cs
+++ Assets/Scripts/ChichenItzaMapProgression.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ChichenItzaMapProgression : MonoBehaviour
 {
     public GameObject player;
+    public GameplayCameraAngles camAngles;
 
     // Pickables
     public GameObject pyramidKey;
@@ -13,6 +14,10 @@ public class ChichenItzaMapProgression : MonoBehaviour
     // Moving elements
     public GameObject camazoSouthWall;
     public GameObject camazoEastWall;
+    public GameObject kukulkansInElCastillo;
+
+    private bool firstSectionComplete;
+    private bool secondSectionComplete;
 
     private void Update()
     {
@@ -25,6 +30,26 @@ public class ChichenItzaMapProgression : MonoBehaviour
             else if (camazoEastWall.activeInHierarchy)
             {
                 if (Vector3.Distance(player.transform.position, camazoEastWall.transform.position) < 2f) camazoEastWall.SetActive(false);
+            }
+        }
+
+        // Placeholder for testing gameplay loop
+        if (!firstSectionComplete)
+        {
+            if (player.transform.position.x < 123f && player.transform.position.z > 0f)
+            {
+                camAngles.SetCameraAngle(1);
+                firstSectionComplete = true;
+            }
+        }
+        else if (!secondSectionComplete)
+        {
+            // pyramid key is placeholder check -> change to camazo icon
+            if (!pyramidKey.activeInHierarchy && !pickableMask.activeInHierarchy && !pickableKukulkans.activeInHierarchy)
+            {
+                camAngles.SetCameraAngle(2);
+                kukulkansInElCastillo.SetActive(true);
+                secondSectionComplete = true;
             }
         }
     }
