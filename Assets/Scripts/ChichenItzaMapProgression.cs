d379f3fdedeafd7ee1f2af19d0e0217dfcff5e96
SummaS99 <LeipuriHiiva1@gmail.com>
2026-05-16 10:41:53 +0300

    add infinite random enemy spawns to chichen itza

--- Assets/Scripts/ChichenItzaMapProgression.cs
+++ Assets/Scripts/ChichenItzaMapProgression.cs
@@ -20,6 +20,10 @@ public class ChichenItzaMapProgression : MonoBehaviour
     // Enemy clusters
     public GameObject firstEnemyCluster;
 
+    // Infinite enemy spawn points
+    public Transform[] forestSpawns;
+    public Transform[] elCastilloSpawns;
+
     private bool firstEnemyAttackStarted;
     private bool firstSectionComplete;
     private bool secondSectionComplete;
@@ -58,6 +62,7 @@ public class ChichenItzaMapProgression : MonoBehaviour
             if (player.transform.position.x < 123f && player.transform.position.z > 0f)
             {
                 camAngles.SetCameraAngle(1);
+                StartCoroutine(SpawnEnemiesInfinitely());
                 firstSectionComplete = true;
             }
         }
@@ -77,5 +82,37 @@ public class ChichenItzaMapProgression : MonoBehaviour
     {
         yield return new WaitForSeconds(5.5f);
         firstEnemyCluster.SetActive(true);
+    }
+
+    private IEnumerator SpawnEnemiesInfinitely()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(4f, 8f));
+
+            Vector3 spawnPoint;
+
+            // 75% ground, 25% forest
+            if (Random.value < 0.75f)
+            {
+                spawnPoint = elCastilloSpawns[Random.Range(0, elCastilloSpawns.Length)].position;
+            }
+            else
+            {
+                spawnPoint = forestSpawns[Random.Range(0, forestSpawns.Length)].position;
+            }
+
+            // Spawn random enemy
+
+            char c = (char)Random.Range('A', 'E');
+
+            string enemyType = "Clubber";
+            if (c.Equals('B')) enemyType = "Runner";
+            if (c.Equals('C')) enemyType = "SpearWarrior";
+            if (c.Equals('D')) enemyType = "AxeWarrior";
+
+            Enemy enemy = ObjectPooler.Instance.SpawnEnemyFromPool(enemyType, spawnPoint, Quaternion.Euler(Vector3.zero));
+            enemy.ResetEnemyAttributes();
+        }
     }
 }
