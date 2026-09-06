9ee129ca9427e6233e3815ad994737fb3541b66c
SummaS99 <LeipuriHiiva1@gmail.com>
2026-09-06 15:31:02 +0300

    update terrains and lvl3 navmesh

---
+++ Assets/Scripts/CamazoCave.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CamazoCave : MonoBehaviour
+{
+    public Camazo camazoInCave;
+
+    private int bonesOnTablesAmount;
+    public GameObject[] bonesOnTables;
+
+    public GameObject giveGoldPopup;
+
+    public GameObject coinStack1;
+    public GameObject coinStack2;
+    public GameObject coinStack3;
+}
